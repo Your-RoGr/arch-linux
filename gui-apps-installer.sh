b7d22c0f1b4b13a71b37e62565baff4fd60d04fb
RoGr <96972624+Your-RoGr@users.noreply.github.com>
2024-04-23 19:00:45 +0500

    v0.2.5c

--- gui-apps-installer.sh
+++ gui-apps-installer.sh
@@ -89,6 +89,7 @@ yay -Sy --noconfirm docker-desktop
 
 echo "Installing PostgreSQL"
 sudo pacman -Sy --noconfirm postgresql
+sudo -u postgres initdb --locale en_US.UTF-8 -D /var/lib/postgres/data
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
